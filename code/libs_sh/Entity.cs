80f07a570cbcd96e187089a51ecee7adb227ad1c
Garrett Brown <grbrown93@sbcglobal.net>
2021-05-26 00:32:15 -0400

    Start entity proxy

---
+++ code/libs_sh/Entity.cs
@@ -0,0 +1,29 @@
+﻿using MoonSharp.Interpreter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starfall
+{
+    [MoonSharpUserData]
+    class SFEntity
+    {
+        public static void Register()
+        {
+            UserData.RegisterProxyType<SFEntity, Entity>(r => new SFEntity(r));
+        }
+
+        Entity ent;
+        public SFEntity(Entity ent)
+        {
+            this.ent = ent;
+        }
+
+        public List<double> getPos()
+        {
+            Vector v = ent.GetPos();
+            return new List<double> {v.x, v.y, v.z};
+        }
+    }
+
+}
