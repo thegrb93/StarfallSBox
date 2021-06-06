24b9614742a751ad0e67e757b08c0f8c30f0a557
Garrett Brown <grbrown93@sbcglobal.net>
2021-06-06 18:25:00 -0400

    Made xpcall take varargs

--- code/kopilua/src/lbaselib.cs
+++ code/kopilua/src/lbaselib.cs
@@ -382,9 +382,12 @@ namespace KopiLua
 		private static int luaB_xpcall (lua_State L) {
 		  int status;
 		  luaL_checkany(L, 2);
-		  lua_settop(L, 2);
-		  lua_insert(L, 1);  /* put error function under function to be called */
-		  status = lua_pcall(L, 0, LUA_MULTRET, 1);
+
+		  lua_pushvalue(L, 2);  /* move error function to the bottom of the stack */
+		  lua_remove(L, 2);
+		  lua_insert(L, 1);
+
+		  status = lua_pcall(L, lua_gettop( L ) - 2, LUA_MULTRET, 1);
 		  lua_pushboolean(L, (status == 0) ? 1 : 0);
 		  lua_replace(L, 1);
 		  return lua_gettop(L);  /* return status + all results */
